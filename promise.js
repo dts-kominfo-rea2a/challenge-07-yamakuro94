7748921bf5dc2e2189995be37fba20d79232a2b3
Yosep Setiawan <yosep.setiawan@yahoo.co.id>
2022-06-12 22:21:13 +0700

    challenge-07-yamakuro94

--- promise.js
+++ promise.js
@@ -1,7 +1,13 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (ekspresi) => {
+  let IXX = await promiseTheaterIXX();
+  let VGC = await promiseTheaterVGC();
+  return [...Object.values(IXX), ...Object.values(VGC)].filter(
+    (a) => a.hasil === ekspresi
+  ).length;
+};
 
 module.exports = {
   promiseOutput,
